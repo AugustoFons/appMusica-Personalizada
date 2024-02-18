b7c9769f1233900f463fa1ad88d594e450192276
AugustoFons <augustofonsdev@gmail.com>
2024-02-18 00:27:35 -0300

    colaborar boton copy

--- src/components/Colaborar.jsx
+++ src/components/Colaborar.jsx
@@ -1,50 +1,66 @@
-import { Divider, ListItem, List, Box, Grid } from '@mui/material'
+import { Divider, ListItem, List, Box, Grid, Typography } from '@mui/material'
+import copiar from '../utils/images/copy.png'
 
 const Colaborar = () => {
 
     return (
-        <Box className='pago' sx={{ width: 360, height: 210, bgcolor: '#ADBE75', borderRadius: '10px', margin: '40px auto',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <List>
-                <ListItem sx={{width:'310px'}}>
-                    <Grid container alignItems="center">
-                        <Grid item>
+        <Box>
+            <Typography variant='body1' sx={{ maxWidth: 700, mx: 'auto', textAlign: 'justify', mt: 2, color: 'green2019d' }}>
+                <span  className="title" sx={{ lineHeight: 1.2 }}>
+                    <strong className="first-letter">S</strong><strong>i te gusta </strong>
+                    lo que hacemos y querés colaborar para que sigamos desarrollando nuestra propuesta artística, lo podés hacer a nuestra cuenta del Banco provincia. 
+                </span>
+            </Typography>
+            <Box className='pago' sx={{ width: 360, height: 210, bgcolor: '#ADBE75', borderRadius: '10px', margin: '32px auto',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <List>
+                    <ListItem sx={{width:'310px'}}>
+                        <Grid container alignItems="center">
+                            <Grid item>
+                                <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
+                                    <strong>ALIAS</strong>
+                                </span>
+                            </Grid>
+                            <Grid item xs>
+                                <Divider variant="middle" component="li" />
+                            </Grid>
+                        </Grid>
+                    </ListItem>
+                    <ListItem>
+                        <Grid container alignItems="center" justifyContent="center">
                             <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
-                                <strong>ALIAS</strong>
+                                berna64y1
+                            </span>
+                            <span className='copy_btn'>
+                                <img src={copiar} alt="copiar" style={{ width: '18px'}} />
                             </span>
                         </Grid>
-                        <Grid item xs>
-                            <Divider variant="middle" component="li" />
+                    </ListItem>
+                    <ListItem sx={{width:'310px'}}>
+                        <Grid container alignItems="center">
+                            <Grid item xs>
+                                <Divider variant="middle" component="li" />
+                            </Grid>
+                            <Grid item>
+                                <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
+                                    <strong>CBU</strong>
+                                </span>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </ListItem>
-                <ListItem>
-                    <Grid container alignItems="center" justifyContent="center">
-                        <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
-                            berna64y1
-                        </span>
-                    </Grid>
-                </ListItem>
-                <ListItem sx={{width:'310px'}}>
-                    <Grid container alignItems="center">
-                        <Grid item xs>
-                            <Divider variant="middle" component="li" />
-                        </Grid>
-                        <Grid item>
+                    </ListItem>
+                    <ListItem>
+                        <Grid container alignItems="center" justifyContent="center">
+                            <span  className='copy_btn'>
+                                <img src={copiar} alt="copiar" style={{ width: '18px' }} />
+                            </span>
                             <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
-                                <strong>CBU</strong>
+                                0140999803200058546799
                             </span>
                         </Grid>
-                    </Grid>
-                </ListItem>
-                <ListItem>
-                    <Grid container alignItems="center" justifyContent="center">
-                        <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
-                            0140999803200058546799
-                        </span>
-                    </Grid>
-                </ListItem>
-            </List>
+                    </ListItem>
+                </List>
+            </Box>
         </Box>
+
 
     )
 }
