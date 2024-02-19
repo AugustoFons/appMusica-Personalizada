2bc2b4643c9be7bf24a58b647d04e7c4e1becd3d
AugustoFons <augustofonsdev@gmail.com>
2024-02-18 23:43:58 -0300

    responsive pagos

--- src/components/Colaborar.jsx
+++ src/components/Colaborar.jsx
@@ -1,19 +1,26 @@
 import { Divider, ListItem, List, Box, Grid, Typography } from '@mui/material'
 import copiar from '../utils/images/copy.png'
+import { useState } from 'react'
 
 const Colaborar = () => {
+    const [copied, setCopied] = useState("");
+    const handleCopy = (text) => {
+        setCopied(text);
+        navigator.clipboard.writeText(text);
+        setTimeout(() => setCopied(""), 3000);
+    };
 
     return (
-        <Box>
+        <Box style={{ width: '100%'}}>
             <Typography variant='body1' sx={{ maxWidth: 700, mx: 'auto', textAlign: 'justify', mt: 2, color: 'green2019d' }}>
                 <span  className="title" sx={{ lineHeight: 1.2 }}>
                     <strong className="first-letter">S</strong><strong>i te gusta </strong>
                     lo que hacemos y querés colaborar para que sigamos desarrollando nuestra propuesta artística, lo podés hacer a nuestra cuenta del Banco provincia. 
                 </span>
             </Typography>
-            <Box className='pago' sx={{ width: 360, height: 210, bgcolor: '#ADBE75', borderRadius: '10px', margin: '32px auto',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box className='pago' sx={{ bgcolor: '#ADBE75', borderRadius: '10px', margin: '32px auto',  display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <List>
-                    <ListItem sx={{width:'310px'}}>
+                    <ListItem sx={{ width: { xs: '250px', sm: '310px' } }}>
                         <Grid container alignItems="center">
                             <Grid item>
                                 <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
@@ -30,12 +37,12 @@ const Colaborar = () => {
                             <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
                                 berna64y1
                             </span>
-                            <span className='copy_btn'>
+                            <span className='copy_btn' onClick={() => handleCopy('berna64y1')}>
                                 <img src={copiar} alt="copiar" style={{ width: '18px'}} />
                             </span>
                         </Grid>
                     </ListItem>
-                    <ListItem sx={{width:'310px'}}>
+                    <ListItem sx={{ width: { xs: '250px', sm: '310px' } }}>
                         <Grid container alignItems="center">
                             <Grid item xs>
                                 <Divider variant="middle" component="li" />
@@ -49,7 +56,7 @@ const Colaborar = () => {
                     </ListItem>
                     <ListItem>
                         <Grid container alignItems="center" justifyContent="center">
-                            <span  className='copy_btn'>
+                            <span  className='copy_btn' onClick={() => handleCopy('0140999803200058546799')}>
                                 <img src={copiar} alt="copiar" style={{ width: '18px' }} />
                             </span>
                             <span className="title" sx={{ lineHeight: 1.2 }} style={{ color: '#516655' }}>
