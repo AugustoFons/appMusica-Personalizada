ad990adcce290cf7e27eae29019b03c54deeeaed
AugustoFons <augustofonsdev@gmail.com>
2024-02-25 00:27:20 -0300

    mejoras redes

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -9,9 +9,9 @@ import amazon from '../utils/images/amz.png'
 
 const Redes = () => {
     return (
-        <Box sx={{ maxWidth: 1000, mt: 2, mx: 'auto'}}>
+        <Box sx={{ maxWidth: 1000, mt: 1, mx: 'auto'}}>
                     <Box style={{ }}>
-                        <Typography variant='h6' sx={{display: 'flex', justifyContent: 'center', textAlign: 'center', color: 'green2019d', my: 4}}>
+                        <Typography variant='h6' sx={{display: 'flex', justifyContent: 'center', textAlign: 'center', color: 'green2019d', my: 3}}>
                             <span className="title" sx={{ lineHeight: 1.2 }}>
                                 ¡Seguime en mis Redes!
                             </span>
@@ -25,7 +25,7 @@ const Redes = () => {
                                 <InstagramIcon fontSize="large" sx={{ color: 'green2019d', transform: 'scale(2.3)', '&:hover': {color: '#9AA69C', cursor: 'pointer', transition: 'color 1s ease, background-color 0.3s ease'}}}/>
                             </span>
                             <span title="YouTube">
-                                <YouTubeIcon fontSize="large" sx={{ color: 'green2019d', transform: 'scale(3)', '&:hover': {color: '#9AA69C', cursor: 'pointer', transition: 'color 1s ease, background-color 0.3s ease'}}}/>
+                                <YouTubeIcon fontSize="large" sx={{ color: 'green2019d', transform: 'scale(2.9)',ml:1.2, xs: {ml:0}, '&:hover': {color: '#9AA69C', cursor: 'pointer', transition: 'color 1s ease, background-color 0.3s ease'}}}/>
                             </span>
                     </Box>
 
